b6a9689f41862bdf21680390d8e7ee7eac499c37
SORU <jinsoru0720@gmail.com>
2020-05-07 23:45:34 +0900

    빅데이터 메인구성

--- resources/views/bigdata/big_data.blade.php
+++ resources/views/bigdata/big_data.blade.php
@@ -2,56 +2,11 @@
 
 @section('content')
 <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-
 <!-- home.blade -->
 @include('bigdata.bigdata_home')
-<!-- googlemap -->
-
-<!-- chart.blade -->
-@include('bigdata.bigdata_chart.chart')
 @endsection
 
 @section('script')
-<script>
-    // datepicker
-    $(function() {	
-        $('#bigdata_date').datepicker({
-            format: "yyyy-mm-dd",	//데이터 포맷 형식
-            autoclose : true,	//사용자가 날짜를 클릭하면 자동 캘린더가 닫히는 옵션
-            calendarWeeks : false, //캘린더 옆에 몇 주차인지 보여주는 옵션
-            clearBtn : false, //날짜 선택한 값 초기화 해주는 버튼 보여주는 옵션 기본값 false 보여주려면 true
-            disableTouchKeyboard : false,	//모바일에서 플러그인 작동 여부 기본값 false 가 작동 true가 작동 안함.
-            immediateUpdates: false,	//사용자가 보는 화면으로 바로바로 날짜를 변경할지 여부 기본값 :false 
-            multidate : false, //여러 날짜 선택할 수 있게 하는 옵션 기본값 :false 
-            multidateSeparator :",", //여러 날짜를 선택했을 때 사이에 나타나는 글짜 2019-05-01,2019-06-01
-            templates : {
-                leftArrow: '&laquo;',
-                rightArrow: '&raquo;'
-            }, //다음달 이전달로 넘어가는 화살표 모양 커스텀 마이징 
-            showWeekDays : true ,// 위에 요일 보여주는 옵션 기본값 : true
-            title: "날짜선택",
-            todayHighlight : true,
-            toggleActive : true,	//이미 선택된 날짜 선택하면 기본값 : false인경우 그대로 유지 true인 경우 날짜 삭제
-            weekStart : 0 ,//달력 시작 요일 선택하는 것 기본값은 0인 일요일 
-            language : "ko"	//달력의 언어 선택, 그에 맞는 js로 교체해줘야한다.
-        });
-    });//ready end
-
-    // 조회버튼 click event
-    $("#bigdata-search").on('click', function() {
-    var bigdata_date = $('#bigdata_date').val();
-    console.log(bigdata_date);
-    $.ajax({
-        headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},  
-        type: 'GET',
-        url: `/bigdata/${bigdata_date}/search/`,
-    }).then(function(data){
-        console.log("조회성공");
-        window.location.href =`/bigdata/${bigdata_date}/search/`;
-    });
-});
-</script>
-
 <!-- chart.js파일 불러오기 -->
 <script src="/js/bigdata/bigdata-chart.js"></script>
 @endsection
--- resources/views/bigdata/bigdata_home.blade.php
+++ resources/views/bigdata/bigdata_home.blade.php
@@ -1,43 +1,63 @@
+
+
+
+
 <div class="card-menu">
-    <div class="col-6 bigdata-card-menu">
+    <div class="col-8 bigdata-card-menu mt-5">
         <p class="bigdata-text">쿠루마모리119에서 제공되는 졸음운전 데이터입니다.</p>
-        <div class="card sleep-chart">
-            <div class="card-header">
-                졸음운전
-            </div>
-            <div class="card-body">
-                <h6 class="card-title">졸음운전</h6>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada.</p>
-                <button id="sleep-detail" class="btn btn-info">자세히 보기</button>
+        <div class="card sleep-chart border-info">
+            <div class="row no-gutters">
+                <div class="col-4">
+                    <img src="images/bigdata/graph1.png" alt="" class="card-img card-img-view">
+                </div>
+                <div class="col-8 text-right">
+                    <div class="card-body">
+                        <h5 class="card-title">졸음운전</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. </p>
+                        <button id="sleep-detail" class="btn btn-info">자세히 보기</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="col-6 bigdata-card-menu">
+    <div class="col-8 bigdata-card-menu mt-5">
         <p class="bigdata-text">쿠루마모리119에서 제공되는 급정거 급가속 데이터입니다.</p>
-        <div class="card sudden-chart">
-            <div class="card-header">
-                급정거 급가속
-            </div>
-            <div class="card-body">
-                <h6 class="card-title">급정거 급가속</h6>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada.</p>
-                <button id="sudden-detail" class="btn btn-info">자세히 보기</button>
+        <div class="card sudden-chart border-warning">
+            <div class="row no-gutters">
+                <div class="col-4">
+                    <img src="images/bigdata/graph2.png" alt="" class="card-img card-img-view">
+                </div>
+                <div class="col-8 text-right">
+                    <div class="card-body">
+                        <h5 class="card-title">급정거 급가속</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. </p>
+                        <button id="sudden-detail" class="btn btn-info">자세히 보기</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="col-6 bigdata-card-menu">
+    <div class="col-8 bigdata-card-menu mt-5">
         <p class="bigdata-text">쿠루마모리119에서 제공되는 교통사고 데이터입니다.</p>
-        <div class="card accident-chart">
-            <div class="card-header">
-                교통사고
-            </div>
-            <div class="card-body">
-                <h6 class="card-title">교통사고</h6>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada.</p>
-                <button id="accident-detail" class="btn btn-info">자세히 보기</button>
+        <div class="card accident-chart border-danger">
+            <div class="row no-gutters">
+                <div class="col-4">
+                    <img src="images/bigdata/graph3.png" alt="" class="card-img card-img-view">
+                </div>
+                <div class="col-8 text-right">
+                    <div class="card-body">
+                        <h5 class="card-title">교통사고</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. </p>
+                        <button id="accident-detail" class="btn btn-info">자세히 보기</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
