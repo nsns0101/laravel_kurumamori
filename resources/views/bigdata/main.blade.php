fe9614d0d7887139779d4aeb76c05761bb6e617e
nsns0101 <nsns0101@naver.com>
2020-05-16 17:26:00 +0900

    css수정

--- resources/views/bigdata/main.blade.php
+++ resources/views/bigdata/main.blade.php
@@ -1,55 +1,62 @@
-<div class="container px-3 py-5 p-md-5">
-    <div class="bigdata-card-menu mt-5">
-        <p class="bigdata-text">쿠루마모리119에서 제공되는 졸음운전 데이터입니다.</p>
-        <div class="card sleep-chart border-info">
-            <div class="row no-gutters">
-                <div class="col-4">
-                    <img src="images/bigdata/graph1.png" alt="" class="card-img card-img-view">
-                </div>
-                <div class="col-8 text-right">
-                    <div class="card-body">
-                        <h5 class="card-title">졸음운전</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. </p>
-                        <a class="btn btn-info" href="{{route('bigdata.show','sleep')}}">자세히 보기</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<section id="intro" class="section intro">
 
-    <div class="bigdata-card-menu mt-5">
-        <p class="bigdata-text">쿠루마모리119에서 제공되는 급가속 급정거 데이터입니다.</p>
-        <div class="card sudden-chart border-warning">
-            <div class="row no-gutters">
-                <div class="col-4">
-                    <img src="images/bigdata/graph2.png" alt="" class="card-img card-img-view">
-                </div>
-                <div class="col-8 text-right">
-                    <div class="card-body">
-                        <h5 class="card-title">급정거 급가속</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. </p>
-                        <a class="btn btn-info" href="/bigdata/sudden">자세히 보기</a>
+<div class="row">
+    <div class="col-md-2"></div>
+    <div class="col-md-8">
+        <div class="bigdata-card-menu mt-5">
+            <p class="bigdata-text">쿠루마모리119에서 제공되는 졸음운전 데이터입니다.</p>
+            <div class="card sleep-chart border-info">
+                <div class="row no-gutters">
+                    <div class="col-4">
+                        <img src="images/bigdata/graph1.png" alt="" class="card-img card-img-view">
+                    </div>
+                    <div class="col-8 text-right">
+                        <div class="card-body">
+                            <h5 class="card-title">졸음운전</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. </p>
+                            <a class="btn btn-info" href="{{route('bigdata.show','sleep')}}">자세히 보기</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="bigdata-card-menu mt-5">
-        <p class="bigdata-text">쿠루마모리119에서 제공되는 교통사고 데이터입니다.</p>
-        <div class="card accident-chart border-danger">
-            <div class="row no-gutters">
-                <div class="col-4">
-                    <img src="images/bigdata/graph3.png" alt="" class="card-img card-img-view">
+        <div class="bigdata-card-menu mt-5">
+            <p class="bigdata-text">쿠루마모리119에서 제공되는 급가속 급정거 데이터입니다.</p>
+            <div class="card sudden-chart border-warning">
+                <div class="row no-gutters">
+                    <div class="col-4">
+                        <img src="images/bigdata/graph2.png" alt="" class="card-img card-img-view">
+                    </div>
+                    <div class="col-8 text-right">
+                        <div class="card-body">
+                            <h5 class="card-title">급정거 급가속</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. </p>
+                            <a class="btn btn-info" href="/bigdata/sudden">자세히 보기</a>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-8 text-right">
-                    <div class="card-body">
-                        <h5 class="card-title">교통사고</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. </p>
-                        <a href="/bigdata/accident"><button id="accident-detail" class="btn btn-info">자세히 보기</button></a>
+            </div>
+        </div>
+
+        <div class="bigdata-card-menu mt-5">
+            <p class="bigdata-text">쿠루마모리119에서 제공되는 교통사고 데이터입니다.</p>
+            <div class="card accident-chart border-danger">
+                <div class="row no-gutters">
+                    <div class="col-4">
+                        <img src="images/bigdata/graph3.png" alt="" class="card-img card-img-view">
+                    </div>
+                    <div class="col-8 text-right">
+                        <div class="card-body">
+                            <h5 class="card-title">교통사고</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas sed sem ut malesuada. </p>
+                            <a href="/bigdata/accident"><button id="accident-detail" class="btn btn-info">자세히 보기</button></a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    <div class="col-md-2"></div>
+</div>    
+</section>
