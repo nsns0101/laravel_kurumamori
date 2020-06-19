3b34ead62ad783784003061e441ff8ed522905d2
nsns0101 <nsns0101@naver.com>
2020-06-19 17:35:54 +0900

    animate...

--- resources/js/routes/Home/Home.js
+++ resources/js/routes/Home/Home.js
@@ -6,6 +6,7 @@ import "./Home.css";
 
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const Wrapper = styled.div`
   min-height: 80vh;
@@ -20,22 +21,27 @@ export default () => {
         
     }
     return (
-        <React.Fragment>
+        <body data-aos-easing="ease" data-aos-duration="1000" data-aos-delay="0">
             <section id="hero" style={hero}>
                 <div className="row mx-0 px-0">
                     <div className="col-lg-1 col-md-1 col-sm-1"/>
                     <div className="col-lg-10 col-md-10 col-sm-10">
                         <div className="row">
                             <div className="col-sm-12 col-lg-6" style={{marginTop:"20%"}}>
-                                <h1 style={{color:"white", fontSize:"40px"}}>くるまもり１１９</h1>
-                                <h3 style={{color:"white", fontSize:"24px"}}>사고를 스스로 인식하는 스마트 디바이스</h3>
-                                <div>
-                                <Link id="btn_get_product" className="btn" to="/products">
-                                    제품 구매하기
-                                </Link>
-                                </div>
+                                <ScrollAnimation animateIn='fadeIn' delay={1000}>
+
+                                    <h1 style={{color:"white", fontSize:"40px"}}>くるまもり１１９</h1>
+                                </ScrollAnimation>
+                                <ScrollAnimation animateIn='fadeIn' delay={1500}>
+                                    <h3 style={{color:"white", fontSize:"24px"}}>사고를 스스로 인식하는 스마트 디바이스</h3>
+                                </ScrollAnimation>
+                                <ScrollAnimation animateIn='fadeIn' delay={2000}>
+                                    <Link id="btn_get_product" className="btn" to="/products">
+                                        제품 구매하기
+                                    </Link>
+                                </ScrollAnimation>
                             </div>
-                            <div className="col-lg-6 col-md-6 col-sm-6 pt-3 align-self-center" style={{marginTop:"10%"}}>
+                            <div className="animated fadeInUp col-lg-6 col-md-6 col-sm-6 pt-3 align-self-center" style={{marginTop:"10%"}}>
                                 <img src="/images/main_image.png" style={{width:"80%"}}/>
                             </div>
                         </div>
@@ -46,7 +52,7 @@ export default () => {
             <Function1/>
             <Function2/>
             <Team/>
-        </React.Fragment>
+        </body>
     
     );
 
--- resources/js/routes/Home/partial/Team.js
+++ resources/js/routes/Home/partial/Team.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Team.css";
+import ScrollAnimation from "react-animate-on-scroll";
+import "animate.css/animate.min.css";
 
 export default () => {
     const main_text = {
@@ -21,76 +23,10 @@ export default () => {
         fontWeight: "bold",
         color:"white",
     }
-    // //팀
-    // const Team = {
-    //     textAlign: "center",
-    //     marginBottom: "20px",
-    //     background: "#343a40",
-    //     position: "relative",
-    // }
-    // const Member_info_content  = {
-    //     position: "absolute",
-    //     left: 0,
-    //     right: 0,
-    //     bottom: "10px",
-    //     transition: "bottom 0.4s"
-    // }
-    // const member_info_content_h4 = {
-    //     fontWeight: "700",
-    //     marginBottom: "2px",
-    //     fontSize: "18px",
-    //     color: "white"
-    // }
-    // const member_info_content_span = {
-    //     fontStyle: "italic",
-    //     display: "block",
-    //     fontSize: "13px",
-    //     color: "white"
-    // }
-    // const member_social = {
-    //     position: "absolute",
-    //     left: 0,
-    //     bottom: "-38px",
-    //     right: 0,
-    //     height: "48px",
-    //     transition: "bottom ease-in-out 0.4s",
-    //     textAlign: "center",
-    // }
-    // const member_social_a = {
-    //     transition: "color 0.3s",
-    //     color: "rgba(255, 255, 255, 0.7)",
-    //     margin: "0 10px",
-    //     display: "inline-block"
-    // }
-    // const member_social_i = {
-    //     fontSize: "18px",
-    //     margin: "0 2px"
-    // }
-    // const img_fluid = {
-    //     maxWidth: "100%",
-    //     height: "auto"
-    // }
 
-    const team_func = (name, position) => {
+    const team_func = (name, position, delay) => {
         return (
-            // <div className="col-lg-4 col-md-4 col-sm-12 text-center aos-init aos-animate mb-3" data-aos="fade-up" data-aos-delay="300">
-            //     <div className="member" style={Team}>
-            //         <img src={`/images/team/${name}.jpg`} alt="img" style={{width:"60%", height:"250px"}}/>
-            //         <div className="member-info">
-            //             <div className="member-info-content" style={Member_info_content}>
-            //                 <h4 className="mt-2" style={member_info_content_h4}>{name}</h4>
-            //                 <span style={member_info_content_span}>{position}</span>
-            //             </div>
-            //             <div className="social" style={member_social}>
-            //                 <a href="" style={member_social_a}><i className="icofont-facebook" style={member_social_i}></i></a>
-            //                 <a href="" style={member_social_a}><i className="icofont-twitter" style={member_social_i}></i></a>
-            //                 <a href="" style={member_social_a}><i className="icofont-instagram" style={member_social_i}></i></a>
-            //                 <a href="" style={member_social_a}><i className="icofont-linkedin" style={member_social_i}></i></a>
-            //             </div>
-            //         </div>
-            //     </div>
-            // </div>
-            <div className="col-lg-4 col-md-4 col-sm-12 text-center aos-init aos-animate mb-3 team" data-aos="fade-up" data-aos-delay="300">
+            <div className="col-lg-4 col-md-4 col-sm-12 text-center aos-init mb-3 team">
                 <div className="member">
                     <img src={`/images/team/${name}.jpg`} alt="img" style={{width:"100%", height:"250px"}}/>
                     <div className="member-info">
@@ -112,29 +48,35 @@ export default () => {
 
     return (
         <section id="intro" className="section intro" style={{background:"#2D405F"}}>
-            <div className="row">
-                <div className="col-lg-2 col-md-2 col-sm-3"/>
+            <ScrollAnimation animateIn='bounceInUp'>                
+                <div className="team">
+                    <div className="row">
+                        <div className="col-lg-2 col-md-2 col-sm-3"/>
 
-                <div className="col-lg-8 col-md-8 col-sm-6">
-                    <div className="row justify-content-around pt-5">
-                        <div className="col-lg-12 col-md-12 col-sm-12 text-center">
-                            <h3 style={main_text}>Team</h3>
-                            <p style={{color:"white", marginBottom:"20px"}}>
-                                Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. 
-                                <br/>Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.
-                            </p>
+                        <div className="col-lg-8 col-md-8 col-sm-6">
+                            <div className="row justify-content-around pt-5">
+                                <div className="col-lg-12 col-md-12 col-sm-12 text-center">
+                                    <h3 style={main_text}>Team</h3>
+
+                                    <p style={{color:"white", marginBottom:"20px"}}>
+                                        Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. 
+                                        <br/>Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.
+                                    </p>
+                                </div>
+                                    {/* 이름, 담당, 딜레이 */}
+                                    {team_func("장준혁", "App", 1)}
+                                    {team_func("예준현", "HardWare", 2)}
+                                    {team_func("정인식", "HardWare", 3)}
+                                    {team_func("김도형", "Web", 4)}
+                                    {team_func("이재영", "Web", 5)}
+                                    {team_func("팽진솔", "Server", 6)}
+                            </div>
                         </div>
-                        {team_func("장준혁", "App")}
-                        {team_func("예준현", "HardWare")}
-                        {team_func("정인식", "HardWare")}
-                        {team_func("김도형", "Web")}
-                        {team_func("이재영", "Web")}
-                        {team_func("팽진솔", "Server")}
+
+                        <div className="col-lg-2 col-md-2 col-sm-3"/>
                     </div>
                 </div>
-
-                <div className="col-lg-2 col-md-2 col-sm-3"/>
-            </div>
+            </ScrollAnimation>
         </section>
     );
 }
