6aafe68c014ca0689e08395172e7074e9b3824aa
nsns0101 <nsns0101@naver.com>
2020-06-23 07:03:48 +0900

    top button

--- resources/js/layout/Footer.js
+++ resources/js/layout/Footer.js
@@ -14,7 +14,9 @@ const Footer = styled.div`
     text-align: center;
     opacity:1;
 `;
-
+const up = () => {
+    window.scrollTo(0,0);
+}
 export default () => {
     return (
         <Footer>
@@ -24,13 +26,24 @@ export default () => {
                         <div className="col-lg-2 col-md-2 col-sm-2">
                             <p style={{fontSize:"16px"}}>© Copyright Miro.</p>
                         </div>
-                        <div className="col-lg-9 col-md-9 col-sm-9">
+                        <div className="col-lg-7 col-md-7 col-sm-7">
 
                         </div>
                         <div className="col-lg-1 col-md-1 col-sm-1">
                             <a href="https://www.youtube.com/channel/UCTLr43DTgWut2lSQ2vFcXAw/featured">
                                 <img src="/icon/youtube.png" style={{width:"30px"}}/>
                             </a>
+
+                        </div>
+                        <div className="col-lg-2 col-md-2 col-sm-2">
+                            <button className="btn btn-success" onClick={()=>up()} style={{
+                                position: "fixed",
+                                // bottom:"70px",
+                                bottom: "10px",
+                                right:"30px",       
+                            }}>
+                               ↑<br/>TOP
+                            </button>
                         </div>
                     </div>
                 </div>
